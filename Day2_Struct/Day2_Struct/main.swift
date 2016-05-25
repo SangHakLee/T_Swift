6b76c72e14ec51d2ed25947edc6f9c1402088d5b
SangHakLee <leesanghak7298@gmail.com>
2016-05-25 14:39:29 +0900

    enum 테스트

--- Day2_Struct/Day2_Struct/main.swift
+++ Day2_Struct/Day2_Struct/main.swift
@@ -101,28 +101,94 @@
 
 
 
-//  상속과 재정의 p118
-class MyClass {
-    var value = 0
+////  상속과 재정의 p118
+//class MyClass {
+//    var value = 0
+//}
+//var obj1 = MyClass()
+//var obj2 = obj1
+//obj1.value = 10
+//print(obj1.value)
+//print(obj2.value)
+//
+//struct MyStruct {
+//    var value = 0
+//}
+//var obj3 = MyStruct()
+//var obj4 = obj3
+//obj3.value = 10
+//print(obj3.value)
+//print(obj4.value)
+
+
+
+////////////////////////
+//enum
+
+
+// 강사님 코드
+enum Pet : String {
+    case Dog = "0"
+    case Cat = "11"
+    var name : String { return "Pet"}
 }
-var obj1 = MyClass()
-var obj2 = obj1
-obj1.value = 10
-print(obj1.value)
-print(obj2.value)
 
-struct MyStruct {
-    var value = 0
+let myPet = Pet.Dog
+print(myPet.rawValue)
+
+let myPet2 = Pet(rawValue: "0") // 옵셔널, 범위를 벗어나 nil을 반환할 수 있으므로
+print(myPet2)
+
+struct Person {
+    var gender : Gender!
 }
-var obj3 = MyStruct()
-var obj4 = obj3
-obj3.value = 10
-print(obj3.value)
-print(obj4.value)
+enum Gender : String {
+    case M = "Male"
+    case F = "Female"
+}
+let g = Gender.M
 
 
 
+// 프로퍼티와 메소드 p121
+enum Pet2 : Int{
+    case Cat = 0
+    case Dog
+    case Other
+    
+    var name : String{
+        switch self {
+        case .Cat:
+            return "고양이"
+        case .Dog:
+            return "강아지"
+        default:
+            return "기타"
+        }
+    }
+    
+    func description() -> String {
+        return self.name
+    }
+}
+var raz = Pet.Cat
+print(raz.name)
+//raz.description() // 왜 에러????????????????
 
+
+
+// 연관 p122
+enum ArrivalStatus{
+    case OnTime
+    case Dalay(Int)
+}
+var flight = ArrivalStatus.Dalay(1) // 연관 할목이 있는 Enum 객체 생성
+switch flight{
+    case .OnTime:
+        print("제때 도착")
+    case .Dalay(let hour):
+        print("지연 시각 \(hour)")
+}
 
 
 
