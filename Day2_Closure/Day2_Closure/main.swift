edb87538df08e1dee23054d23f58ed4d04c11b2f
SangHakLee <leesanghak7298@gmail.com>
2016-05-25 17:26:23 +0900

    클로져 테스트2

--- Day2_Closure/Day2_Closure/main.swift
+++ Day2_Closure/Day2_Closure/main.swift
@@ -67,17 +67,58 @@ func sayHello() -> String {
 
 
 
-// 클로저 표현식 p134
-var array = [2,3,1,5,4]
-let a1 = array.sort { (a: Int, b:Int) -> Bool in
-    return a < b
-}
-let a2 = array.sort({a,b -> Bool in return a < b })
-let a3 = array.sort({a,b -> Bool in a < b})
-let a4 = array.sort({a,b in a < b})
-let a5 = array.sort({ $0 < $1})
-let a6 = array.sort { $0 < $1 }
+//// 클로저 표현식 p134
+//var array = [2,3,1,5,4]
+//let a1 = array.sort { (a: Int, b:Int) -> Bool in
+//    return a < b
+//}
+//let a2 = array.sort({a,b -> Bool in return a < b })
+//let a3 = array.sort({a,b -> Bool in a < b})
+//let a4 = array.sort({a,b in a < b})
+//let a5 = array.sort({ $0 < $1})
+//let a6 = array.sort { $0 < $1 }
+//
+//
+//
+//
+//func greeting() -> () -> Void{
+//    return { ()-> Void in print("Closure")}
+//}
+//greeting()()
+//
+//func add(i : Int, _ j : Int, handler : ()) -> Int{
+//    return i + j
+//}
+////add(1, 2, {(result in print("1 + 2 = \(result)")})
 
+
+
+
+
+
+
+
+
+// 클로저 반환 p135
+//파라미터와 반환값이 없는 함수 - 함수 타입 : ()->()
+func sayGoodmorning(){
+    print("굿모닝")
+}
+// ()->() 를 반환 타입으로 정의한 함수
+func greeting() ->( ()-> () ) {
+    return sayGoodmorning
+}
+//함수 호출 결과는 타입이 ()->()인 함수
+let ret = greeting()
+ret()
+
+// 클로저 파라미터
+func add(i : Int, _ j: Int ) -> Int {
+    let sum = i + j
+    return sum
+}
+let ret33 = add(1,2)
+print("1 + 2 = \(ret33)")
 
 
 
